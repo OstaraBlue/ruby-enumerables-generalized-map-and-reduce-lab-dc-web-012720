5656e9c93e2ba06ae4add5157b404ddf25e89478
Sonata Arrington <sarrington1009@gmail.com>
2020-01-26 06:36:19 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,18 +11,18 @@ end
 
 
 def reduce(source_array, starting_point = nil)
-   if starting_point
+  if starting_point
      num1 = starting_point
      counter = 0 
-    else
+  else
      num1 = source_array[0]
      counter = 1 
-    end
+  end
     
-    while counter < source_array.counter
+  while counter < source_array.counter
       sum = yield(sum, source_array[counter])
-      counter +=
-    end
-    sum
+      counter += 1
+  end
+  sum
 end
   
