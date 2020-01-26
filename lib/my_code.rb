208e71a733b548db27f795f9544d4bfb62f8ade1
Sonata Arrington <sarrington1009@gmail.com>
2020-01-26 06:00:46 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,7 +10,13 @@ def map(source_array)
 end
 
 
-def reduce(source_array, starting_point)
-
+def reduce(source_array, starting_point =0)
+  total = source_array[0]
+  counter = 0 
+  while counter < source_array.count do 
+    total = yield(total, source_array[counter])
+    counter += 1 
+  end
+  total
 end
   
