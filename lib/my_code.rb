762a51a8eb20241abe3fb3c33b8774aeea5e9c3b
Sonata Arrington <sarrington1009@gmail.com>
2020-01-26 05:53:13 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,7 +11,7 @@ end
 
 
 def reduce(source_array, starting_point =0)
-  total = 
+  total = source_array[counter]
   counter = 0 
   while counter < source_array.count do 
     total = yield(total, source_array[counter])
